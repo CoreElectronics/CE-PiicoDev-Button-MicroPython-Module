7fc477064d5c0eeb440d73a3d4838bc8d6fb2a7d
CoreProduction2 <production2.inbox@coreelectronics.com.au>
2022-07-04 16:59:43 +1000

    Update PiicoDev_Switch.py

--- PiicoDev_Switch.py
+++ PiicoDev_Switch.py
@@ -1,4 +1,4 @@
-# A simple class to read data from the Core Electronics PiicoDev Potentiometer
+# A simple class to read data from a Core Electronics PiicoDev switch
 # Peter Johnston at Core Electronics
 # 2022 APR 06 - Initial release
 
@@ -9,39 +9,41 @@ compat_str = '\nUnified PiicoDev library out of date.  Get the latest module: ht
 _BASE_ADDRESS = 0x42
 _DEVICE_ID    = 409
 
-_REG_STATUS      = 0x01
-_REG_FIRM_MAJ    = 0x02
-_REG_FIRM_MIN    = 0x03
-_REG_I2C_ADDRESS = 0x04
-_REG_PRESS_COUNT = 0x05
-_REG_LED         = 0x07
-_REG_DEBUG       = 0x08
-_REG_DEV_ID      = 0x11
+_REG_STATUS                = 0x01
+_REG_FIRM_MAJ              = 0x02
+_REG_FIRM_MIN              = 0x03
+_REG_I2C_ADDRESS           = 0x04
+_REG_PRESS_COUNT           = 0x05
+_REG_LED                   = 0x07
+_REG_STATE                 = 0x08
+_REG_DOUBLE_CLICK_DETECTED = 0x09
+_REG_DEV_ID                = 0x11
+_REG_DEBUG                 = 0x12
 _REG_DOUBLE_CLICK_DURATION = 0x21
-_REG_DEBOUNCE_WINDOW = 0x23
-_REG_DOUBLE_CLICK_DURATION_OUT = 0x31
-_REG_DEBOUNCE_WINDOW_OUT = 0x33
+_REG_DEBOUNCE_WINDOW       = 0x23
 
-
-def _readBit(x, n):
+def _read_bit(x, n):
     return x & 1 << n != 0
+
+def _set_bit(x, n):
+    return x | (1 << n)
 
 class PiicoDev_Switch(object):
     @property
     def double_click_duration(self):
-        return self._readInt(_REG_DOUBLE_CLICK_DURATION_OUT, 2)
+        return self._readInt(_REG_DOUBLE_CLICK_DURATION, 2)
     
     @double_click_duration.setter
     def double_click_duration(self, value):
-        self._writeInt(_REG_DOUBLE_CLICK_DURATION, value, 2)
+        self._writeInt(_set_bit(_REG_DOUBLE_CLICK_DURATION, 7), value, 2)
         
     @property
     def debounce_window(self):
-        return self._readInt(_REG_DEBOUNCE_WINDOW_OUT, 2)
+        return self._readInt(_REG_DEBOUNCE_WINDOW, 2)
     
     @debounce_window.setter
     def debounce_window(self, value):
-        self._writeInt(_REG_DEBOUNCE_WINDOW, value, 2)
+        self._writeInt(_set_bit(_REG_DEBOUNCE_WINDOW, 7), value, 2)
     
     def __init__(self, bus=None, freq=None, sda=None, scl=None, address=_BASE_ADDRESS, id=None, double_click_duration=300, debounce_window=40):
         try:
@@ -57,7 +59,6 @@ class PiicoDev_Switch(object):
         self.debounce_window = debounce_window
         self.last_command_known = False
         self.last_command_success = False
-        self.double_click_detected = False
         if type(id) is list and not all(v == 0 for v in id): # preference using the ID argument. ignore id if all elements zero
             assert _max(id) <= 1 and _min(id) >= 0 and len(id) == 4, "id must be a list of 1/0, length=4"
             self.address=8+id[0]+2*id[1]+4*id[2]+8*id[3] # select address from pool
@@ -109,9 +110,8 @@ class PiicoDev_Switch(object):
     def readStatus(self):
         sts=self._readInt(_REG_STATUS)
         if sts is not None:
-            self.last_command_known = _readBit(sts, 2)
-            self.last_command_success = _readBit(sts, 1)
-            self.double_click_detected = _readBit(sts, 3)
+            self.last_command_known = _read_bit(sts, 2)
+            self.last_command_success = _read_bit(sts, 1)
     
     def readID(self):
         x=self._readInt(_REG_DEV_ID, 2)
@@ -126,12 +126,22 @@ class PiicoDev_Switch(object):
     def pwrLED(self, x):
         self._writeInt(_REG_LED, int(x)); return 0
             
-    def read(self, raw=False):
+    def read(self):
         raw_value = self._readInt(_REG_PRESS_COUNT, 2)
         self.readStatus()
         if raw_value is None:
             return(float('NaN'))
-        if raw:
-            return raw_value
         else:
             return raw_value
+        
+    def read_state(self):
+        if self._readInt(_REG_STATE, 1) == 1:
+            return False
+        else:
+            return True
+        
+    def double_click_detected(self):
+        if self._readInt(_REG_DOUBLE_CLICK_DETECTED, 1) == 1:
+            return True
+        else:
+            return False
