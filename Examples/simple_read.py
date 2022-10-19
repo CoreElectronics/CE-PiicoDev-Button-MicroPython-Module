15445e44045edecfb75d642c64eaf372cb61d148
CoreProduction2 <production2.inbox@coreelectronics.com.au>
2022-10-19 15:18:18 +1100

    Update simple_read.py

--- Examples/simple_read.py
+++ Examples/simple_read.py
@@ -1,10 +1,10 @@
 from PiicoDev_Switch import PiicoDev_Switch
 from PiicoDev_Unified import sleep_ms
  
-button = PiicoDev_Switch(debounce_window=90, double_click_duration=2000)   # Initialise the RFID module
+button = PiicoDev_Switch()   # Initialise the module
 
 while True:
-    if button.press_count:
+    if button.was_pressed:
         print(1)
     else:
         print(0)
