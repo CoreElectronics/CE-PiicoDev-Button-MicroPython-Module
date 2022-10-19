9df22f06eb5596ea554eb534ab31801171f12e7d
CoreProduction2 <production2.inbox@coreelectronics.com.au>
2022-10-19 15:18:08 +1100

    Update led_toggle_and_auto_when_double_click.py

--- Examples/led_toggle_and_auto_when_double_click.py
+++ Examples/led_toggle_and_auto_when_double_click.py
@@ -3,10 +3,20 @@ from PiicoDev_Unified import sleep_ms
  
 button = PiicoDev_Switch()   # Initialise the module
 
-while True:
-    while button.was_pressed == False:
-        sleep_ms(10)
-    button.led = False
-    while button.was_pressed == False:
-        sleep_ms(10)
+def auto_toggle():
     button.led = True
+    for x in range(10):
+        sleep_ms(100)
+        button.led = False
+        sleep_ms(100)
+        button.led = True
+
+button.led = True
+while True:
+    if button.was_pressed:
+        button.led = not button.led
+    
+    if button.was_double_pressed:
+        auto_toggle()
+    
+    sleep_ms(10)
